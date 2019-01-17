524adc985236204d98a384818e361205a620ba64
Nam Tran <tranngocnam97@gmail.com>
2019-01-17 17:12:26 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class ExampleSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands
+
+  DoubleSolenoid saltenoid = new DoubleSolenoid(0,1);
+
+  public ExampleSubsystem() {
+    // stuff goes here
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void goOut(){
+    saltenoid.set(DoubleSolenoid.Value.kForward);
+  } 
+  public void goIn(){
+    saltenoid.set(DoubleSolenoid.Value.kReverse);
+  }
+  public void goOff(){
+    saltenoid.set(DoubleSolenoid.Value.kOff);
+  }
+  
+}
